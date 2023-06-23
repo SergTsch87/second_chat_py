350cf1670b3cb6f30b93a4be6e9e292e55e0fc70
unknown <sergtsch87@gmail.com>
2023-06-23 09:48:16 +0300

    while True; close conn and server

--- server.py
+++ server.py
@@ -80,3 +80,25 @@ the program"""
 def remove(connection):
     if connection in list_of_clients:
         list_of_clients.remove(connection)
+
+
+while True:
+    """Accepts a connection request and stores two parameters,
+    conn which is a socket object for that user, and addr
+    which contains the IP address of the client that just
+    connected"""
+    conn, addr = server.accept()
+
+    """Maintains a list of clients for ease of broadcasting
+    a message to all available people in the chatroom"""
+    list_of_clients.append(conn)
+
+    # prints the address of the user that just connected
+    print(f"{addr[0]} connected")
+
+    # creates and individual thread for every user
+    # that connects
+    start_new_thread(clientthread, (conn, addr))
+
+conn.close()
+server.close()
