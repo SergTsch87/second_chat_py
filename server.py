1dd8d08634860b996e95583f6dc1367fee934a69
unknown <sergtsch87@gmail.com>
2023-06-23 09:06:19 +0300

    Add 2 arguments: AF_INET and SOCK_STREAM

--- server.py
+++ server.py
@@ -3,3 +3,10 @@ import socket, select, sys
 '''Replace "thread" with "_thread" for python 3'''
 from _thread import *
 
+"""The first argument AF_INET is the address domain of the
+socket. This is used when we have an Internet Domain with
+any two hosts The second argument is the type of socket.
+SOCK_STREAM means that data or characters are read in
+a continuous flow."""
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
