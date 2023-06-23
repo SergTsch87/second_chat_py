5b8ce9ae1cb64721bc570a2fca6f6cf775dcfcf7
unknown <sergtsch87@gmail.com>
2023-06-23 09:45:42 +0300

    def broadcast(message, connection)

--- server.py
+++ server.py
@@ -57,3 +57,18 @@ def clientthread(conn, addr):
         
         except:
             continue
+
+
+"""Using the below function, we broadcast the message to all
+clients who's object is not the same as the one sending
+the message """
+def broadcast(message, connection):
+    for clients in list_of_clients:
+        if clients != connection:
+            try:
+                clients.send(message)
+            except:
+                clients.close()
+
+                #  if the link is broken, we remove the client
+                remove(clients)
