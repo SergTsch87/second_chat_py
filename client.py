e507f0f62875e0eaebac9b648d810462224fc254
unknown <sergtsch87@gmail.com>
2023-06-24 11:25:06 +0300

    Initial variables

--- client.py
+++ client.py
@@ -1,3 +1,11 @@
 # Python program to implement client side of chat room
 import socket, sys, select
 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if len(sys.argv) != 3:
+    print("Correct usage: script, IP address, port number")
+    exit()
+
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
+server.connect((IP_address, Port))
